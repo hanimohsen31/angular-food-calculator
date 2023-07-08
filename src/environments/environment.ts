84c071f4cf6e2cf84d38aa192b0b78deac248659
Hani Rashed <hrashed@getgroup.com>
2023-07-09 02:23:05 +0300

    'new-deploy'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,25 @@
 export const environment = {
   firebase: {
-    projectId: 'food-calculator-300',
-    appId: '1:276097975981:web:b4f610be1576ad14672824',
-    databaseURL: 'https://food-calculator-300-default-rtdb.firebaseio.com',
-    storageBucket: 'food-calculator-300.appspot.com',
-    apiKey: 'AIzaSyAB2S_y8MJfxKO6ARxv2C0xDdQkUE36jhw',
-    authDomain: 'food-calculator-300.firebaseapp.com',
-    messagingSenderId: '276097975981',
+    apiKey: "AIzaSyCSGV6WbkPWDKzJHRnr5bXDHuq3H5daVf8",
+    authDomain: "food-calculator-500.firebaseapp.com",
+    databaseURL: "https://food-calculator-500-default-rtdb.firebaseio.com",
+    projectId: "food-calculator-500",
+    storageBucket: "food-calculator-500.appspot.com",
+    messagingSenderId: "71693719279",
+    appId: "1:71693719279:web:d8c4d03547b113efc36d1a"
   },
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyAB2S_y8MJfxKO6ARxv2C0xDdQkUE36jhw',
-    authDomain: 'food-calculator-300.firebaseapp.com',
-    databaseURL: 'https://food-calculator-300-default-rtdb.firebaseio.com',
-    projectId: 'food-calculator-300',
-    storageBucket: 'food-calculator-300.appspot.com',
-    messagingSenderId: '276097975981',
-    appId: '1:276097975981:web:b4f610be1576ad14672824',
+    apiKey: "AIzaSyCSGV6WbkPWDKzJHRnr5bXDHuq3H5daVf8",
+    authDomain: "food-calculator-500.firebaseapp.com",
+    databaseURL: "https://food-calculator-500-default-rtdb.firebaseio.com",
+    projectId: "food-calculator-500",
+    storageBucket: "food-calculator-500.appspot.com",
+    messagingSenderId: "71693719279",
+    appId: "1:71693719279:web:d8c4d03547b113efc36d1a"
   },
   database: {
-    url: 'https://food-calculator-300-default-rtdb.firebaseio.com',
+    url: 'https://food-calculator-500-default-rtdb.firebaseio.com',
   },
   admins: ['hanimohsen3131@gmail.com'],
   // recaptchaEnterpriseKey:"F1EC7F2E-C880-4534-9530-6870CB44E0B0"
