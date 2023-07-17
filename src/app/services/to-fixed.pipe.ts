6c062691e7af929c1eb44485bf32c7d41cf815cc
Hani Rashed <hrashed@getgroup.com>
2023-07-17 10:58:55 +0300

    'popup'

--- src/app/services/to-fixed.pipe.ts
+++ src/app/services/to-fixed.pipe.ts
@@ -7,8 +7,12 @@ export class ToFixedPipe implements PipeTransform {
   transform(value: any): any {
     if (typeof value == 'number') {
       return value.toFixed(2);
-    } else if (typeof value == 'string' && value.length > 3) {
+    } else if (typeof +value == 'number') {
       return value.slice(0, 2);
+    } else if (typeof value == 'string') {
+      console.log("toFixed: ",value)
+      let values = value.split(".")
+      return values[0] + values[1].slice(0,2)
     } else {
       return value;
     }
